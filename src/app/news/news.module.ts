04892ce9691a27016ebeaf3f199259aab1c75382
vbSHYAM <shyamvb98@gmail.com>
2023-04-18 10:51:34 +0530

    updated

--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListnewsComponent } from './containers/listnews/listnews.component';
+// import { ListnewsComponent } from './containers/listnews/listnews.component';
 import { ViewnewsComponent } from './containers/viewnews/viewnews.component';
-import { ConstantsComponent } from './constants/constants/constants.component';
-import { NewsPanelComponent } from './containers/news-panel/news-panel.component';
-
 
 
 @NgModule({
   declarations: [
-    ListnewsComponent,
+    // ListnewsComponent,
     ViewnewsComponent,
-    ConstantsComponent,
-    NewsPanelComponent
+    // ConstantsComponent,
   ],
   imports: [
     CommonModule
